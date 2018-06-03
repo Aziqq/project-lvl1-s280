554eaf7fdf3d1db7c984c0bb2fd35096a73491e1
Aziqq <aziqqq@gmail.com>
2018-06-03 09:51:35 +0600

    refactor

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,16 +4,16 @@ import getRandomNum from '../utils';
 const gameDescription = 'Is this number prime?';
 
 const isPrime = (num) => {
-  for (let i = 2; i < num; i += 1) {
+  for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) {
     if (num % i === 0) {
       return false;
     }
   }
-  return true;
+  return num !== 1;
 };
 
 const task = () => {
-  const num = getRandomNum(2, 200);
+  const num = getRandomNum(1, 3);
   const question = num;
   const correctAnswer = isPrime(num) ? 'yes' : 'no';
   return [question, correctAnswer];
